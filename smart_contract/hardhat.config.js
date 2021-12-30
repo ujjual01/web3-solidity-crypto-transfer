d6460eb18d4338ccfb88fb05b215e199657982de
0xuj <44235062+ujjual01@users.noreply.github.com>
2021-12-30 13:30:52 +0530

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.4",
   networks:  {
     ropsten:  {
-      url:'https://eth-ropsten.alchemyapi.io/v2/xe9oZawLjH92MGASwtpBl2s14N9G5CUA',
-      accounts:['79111f9edc503e382f1870a51e7b31123182ec4056ff8d1ea54302108f47c93e']
+      url:'ALCHEMY LINK',
+      accounts:['PRIVATE_KEY']
     }
   }
 };
